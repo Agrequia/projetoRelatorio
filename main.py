c75e405f02a25159469c3c68d64f90e3fdff9a1d
Arthur Bortolanza <athurrodrigueslima@gmail.com>
2026-04-28 08:09:45 -0300

    Adição do geckodriver e teste hello world

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+
+service = Service(executable_path="geckodriver.exe")
+
+driver = webdriver.Firefox(service=service)
+driver.get("http://localhost")
+
+input("Pressione Enter para fechar...")
+driver.quit()
