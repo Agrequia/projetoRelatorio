1ffb05521b8865472cc6b9b7aaf895b24e6a39c7
Arthur Bortolanza <athurrodrigueslima@gmail.com>
2026-04-29 07:34:52 -0300

    Sistema realizando login e acessando a página do primeiro relatório

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
+import time
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+service = Service(executable_path="geckodriver.exe")
+driver = webdriver.Firefox(service=service)
+
 driver.get("https://localhost:8080/HAND/")
 
-input("Pressione Enter para fechar...")
-driver.quit()
+time.sleep(3)
+
+# Preenche o login
+driver.find_element(By.XPATH, "//input[@placeholder='Login']").send_keys("6")
+
+# Preenche a senha + Enter
+driver.find_element(By.XPATH, "//input[@placeholder='Senha']").send_keys("6" + Keys.ENTER)
+
+time.sleep(5)
+
+# Acessa a página onde sera configurado o relatório
+driver.get("https://localhost:8080/HAND/pages/nfe/gerenciamento/search/searchGerenciamentoNfe.xhtml")
