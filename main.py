6446235ce3576a11c9122b7b5ab8c7211f209db6
Arthur Bortolanza <athurrodrigueslima@gmail.com>
2026-04-28 23:00:07 -0300

    Atualização do geckodriver e alteração na main: editado link de acesso e adição do webdriver-manager

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-service = Service(executable_path="geckodriver.exe")
-
-driver = webdriver.Firefox(service=service)
-driver.get("http://localhost")
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+driver.get("https://localhost:8080/HAND/")
 
 input("Pressione Enter para fechar...")
 driver.quit()
