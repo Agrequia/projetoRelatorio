a37f7577fe4ee0cbe269839c0b310c95a4d768c3
Arthur Bortolanza <arthurrodrigueslima@gmail.com>
2026-05-02 12:17:42 -0300

    Configuração do envio por email

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException 
 from datetime import datetime, timedelta
+from email.message import EmailMessage
 import time
 import os
+import smtplib
 
 def estaLogado(driver):
     try:
@@ -37,6 +39,39 @@ def obtem_arquivos_recentes(pasta, extensoes=(".zip", ".pdf")):
 
     return arquivos
 
+def enviar_email(remetente, senha_app, destinatario, zip_path, pdf_path):
+    msg = EmailMessage()
+    msg['Subject'] = 'Relatórios Mensais'
+    msg['From'] = remetente
+    msg['To'] = destinatario
+
+    msg.set_content('Bom dia\n\nSegue em anexo os relatórios mensais.\n\nAtt.')
+
+    # Anexa zip
+    with open(zip_path, 'rb') as f:
+        msg.add_attachment(
+            f.read(),
+            maintype = 'application',
+            subtype = 'zip',
+            filename = os.path.basename(zip_path)
+        )
+
+    # Anexa o pdf
+    with open(pdf_path, 'rb') as f:
+        msg.add_attachment(
+            f.read(),
+            maintype = 'application',
+            subtype = 'pdf',
+            filename = os.path.basename(pdf_path)
+        )
+    
+    # Envia o email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(remetente, senha_app)
+        smtp.send_message(msg)
+
+    print("Email enviado com sucesso!")
+
 driver = criar_driver()
 wait = WebDriverWait(driver, 10) #Todos os time.sleep serao substituidos por isso futuramente
 
@@ -51,6 +86,9 @@ ultimo_dia_mes_anterior = primeiro_dia_mes_atual - timedelta(days=1)
 
 # Primeiro dia do mês anterior
 primeiro_dia_mes_anterior = ultimo_dia_mes_anterior.replace(day=1)
+
+# Mes anterior formato mm-yyyy (para renomear os arquivos baixados)
+mes_ref = primeiro_dia_mes_anterior.strftime("%m-%Y") 
 
 # Formata no padrao dd/mm/yyyy
 data_inicio = primeiro_dia_mes_anterior.strftime("%d/%m/%Y")
@@ -115,8 +153,8 @@ if "ui-state-active" not in checkbox.get_attribute("class"):
 
 time.sleep(3)
 
-# Pressiona exportar notas
-driver.find_element(By.ID, "j_idt332").click()
+# Pressiona exportar notas -- mudou o id
+driver.find_element(By.ID, "j_idt338").click()
 
 time.sleep(10)
 
@@ -166,7 +204,7 @@ dropDown.select_by_visible_text("Venda com NFC-e")
 # Pressiona PDF
 driver.find_element(By.ID, "j_idt62:j_idt65:j_idt76").click()
 
-time.sleep(5)
+time.sleep(10)
 
 ### Identificar e renomear os arquivos ###
 # Local dos arquivos baixados
@@ -179,7 +217,21 @@ arquivos = obtem_arquivos_recentes(pasta_downloads)
 zip_file = next(f for f in arquivos if f.endswith(".zip"))
 pdf_file = next(f for f in arquivos if f.endswith(".pdf"))
 
+# Novo nome dos arquivos
+novo_zip = os.path.join(pasta_downloads, f"NFE_{mes_ref}.zip")
+novo_pdf = os.path.join(pasta_downloads, f"NFCe_{mes_ref}.pdf")
 
+# Renomeia os arquivos
+os.rename(zip_file, novo_zip)
+os.rename(pdf_file, novo_pdf)
+
+time.sleep(10)
 
 ### Enviar para contadora ###
-
+enviar_email(
+    remetente = "mariabombomcco@gmail.com",
+    senha_app = os.getenv("EMAIL_SENHA"),
+    destinatario = "arthurrodrigueslima@gmail.com",
+    zip_path = novo_zip,
+    pdf_path = novo_pdf
+)
